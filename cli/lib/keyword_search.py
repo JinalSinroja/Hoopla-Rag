2147920532b7f625ab4cf13d7b6d2a2c54bef402
Jinal Sinroja <jinalsinroja20@gmail.com>
2026-04-30 09:59:11 +0000

    chapter 1 completed

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -3,8 +3,14 @@ from pathlib import Path
 
 PROJECT_ROOT =  Path(__file__).resolve().parents[2]
 DATA_PATH = PROJECT_ROOT /"data"/"movies.json"
+STOPWORDS_PATH = PROJECT_ROOT /"data"/"stopwords.txt"
 
 def load_movies():
     with open(DATA_PATH, "r") as f:
         data = json.load(f)
     return data["movies"]
+
+def load_stopwords():
+    with open(STOPWORDS_PATH, "r") as f:
+        data = f.read().splitlines()
+    return data
--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -1,10 +1,42 @@
-from lib.keyword_search import load_movies
+from lib.keyword_search import load_movies, load_stopwords
+import string
+from nltk.stem import PorterStemmer
+
+steammer = PorterStemmer()
+def clean_text(text):
+    text = text.lower()
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    return text
+
+def tokenize(text):
+    text = clean_text(text)
+    stopwords = load_stopwords()
+    def _filter(tok):
+        if tok and tok not in stopwords:
+            return True
+        return False
+    
+    res = []
+    for tok in text.split():
+        if _filter(tok):
+            token = steammer.stem(tok)
+            res.append(token)
+    return res
+
+def has_matching_token(query_tokens, movie_tokens):
+    for qtok in query_tokens:
+        for mtok in movie_tokens:
+            if qtok in mtok:
+                return True
+    return False
 
 def search_command(query, n_results):
     movies = load_movies()
     res = []
+    query_tokens = tokenize(query)
     for movie in movies:
-        if query in movie["title"]:
+        movie_tokens = tokenize(movie["title"])
+        if has_matching_token(query_tokens, movie_tokens):
             res.append(movie)
         if len(res) == n_results:
             break
